3a12f01ea340e9947f560fcf9a6074c490354599
Liza <elizabethhoncharenko@gmail.com>
2020-10-30 12:48:06 +0200

    Updates

--- precache-manifest.9c5d9b222df9b272ae4249617f2422f6.js
+++ precache-manifest.9c5d9b222df9b272ae4249617f2422f6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cb5e6e00bb7f2fbd81c66062aa445bee",
+    "revision": "8f59c228ad55c40269af9a9d9c70dc0b",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.67a0cd86.chunk.css"
   },
   {
-    "revision": "aff296c5aac59175c75c",
+    "revision": "6b8cfbeab7b6b3b6eff7",
     "url": "./static/css/main.9f604ba9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.d10b847f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aff296c5aac59175c75c",
-    "url": "./static/js/main.e0c367cb.chunk.js"
+    "revision": "6b8cfbeab7b6b3b6eff7",
+    "url": "./static/js/main.58b7000c.chunk.js"
   },
   {
     "revision": "79d17474afd20bce64b6",
